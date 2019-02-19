a3dc27a6783fc38037441a4fed6b1572e5564938
Arin <arin_muk@hotmail.com>
2019-02-19 18:52:31 -0500

    60 percent complete

---
+++ marsapp.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+from bs4 import BeautifulSoup
+import requests
+import os
+import pandas as pd
+import textwrap
+import pymongo
+import datetime as datetime
+
+app = Flask(__name__)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_data")
+mars_data={}
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db = client.mars_data
+mars_scrape_col = db.mars_scrape.find()
+
+
+@app.route('/')
+def index():
+    marsdata = mongo.db.mars_scrape.find_one()
+    return render_template('index.html', marsdata=marsdata)
+
+
+@app.route('/scrape')
+def scrape():
+    mars = mongo.db.mars_scrape
+    data = scrape_mars.scrape()
+    mars.update(
+        {},
+        data,
+        upsert=True
+    )
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
